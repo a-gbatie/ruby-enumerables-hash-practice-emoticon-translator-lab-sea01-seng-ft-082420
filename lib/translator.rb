8bfa39095e54f49c5d6c0c52bcb72f9572b8251a
Ange Batie <ange.batie@gmail.com>
2020-08-20 05:04:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,7 +31,6 @@ def get_english_meaning(file_path, jap_emoticon)
     if emoticons[key][:japanese] == jap_emoticon
       return emoticons[key][:english]
     end
-    value
   end
     return "Sorry, that emoticon was not found"
 end
