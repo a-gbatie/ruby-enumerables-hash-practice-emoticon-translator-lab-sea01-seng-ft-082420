f5bcf8dfab28a2f32f4edd871298f69a1bbf826c
Ange Batie <ange.batie@gmail.com>
2020-08-20 04:45:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 def get_japanese_emoticon(file_path, eng_emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key, value|
-    if emoticons[key]
+    if emoticons[key][:english] == eng_emoticon
     
 end
 
